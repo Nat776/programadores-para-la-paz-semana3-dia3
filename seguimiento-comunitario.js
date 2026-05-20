167f2cc168cc9b44092861452e39bee6abef1daa
natalia martinez <nataliamartinezsuarez33@gmail.com>
2026-05-20 00:06:30 -0500

    Ciclos while en JavaScript semana 3 dia 3

---
+++ seguimiento-comunitario.js
@@ -0,0 +1,12 @@
+let semanaActual = 1
+const semanasDeSeguimiento = 4
+let propuestasAtendidas = 3
+
+console.log("Seguimiento comunitario del programa")
+
+while (semanaActual <= semanasDeSeguimiento) {
+  console.log("Semana", semanaActual, "- Propuestas atendidas:", propuestasAtendidas)
+  semanaActual = semanaActual + 1
+}
+
+console.log("Proceso de seguimiento comunitario finalizado.")
